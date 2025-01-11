a17b82a3998b9d5c42d305af23443ff2698bca69
CarterSnich <jmestreraclosa@gmail.com>
2025-01-11 21:33:01 +0800

    added service worker

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,3 +1,22 @@
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("sw.js", {
+        scope: "./",
+      });
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
 const searchEngines = {
   google: "https://google.com/search?q=",
   duckduckgo: "https://duckduckgo.com/?q=",
@@ -65,3 +84,5 @@ searchBarForm.addEventListener("submit", function (event) {
 
   window.location.href = url + query;
 });
+
+registerServiceWorker();
