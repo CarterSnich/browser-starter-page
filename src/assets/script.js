34b552ea6cd17742af0e149163262eb30245b0e9
CarterSnich <jmestreraclosa@gmail.com>
2025-01-21 23:37:36 +0800

    removed search bar

--- src/assets/script.js
+++ src/assets/script.js
@@ -17,31 +17,10 @@ const registerServiceWorker = async () => {
   }
 };
 
-const searchEngines = {
-  google: "https://google.com/search?q=",
-  duckduckgo: "https://duckduckgo.com/?q=",
-  youtube: "https://www.youtube.com/results?search_query=",
-  wikipedia: "https://en.wikipedia.org/wiki/Special:Search?go=Go&search=",
-  reddit: "https://www.reddit.com/search/?q=",
-  aur: "https://aur.archlinux.org/packages?K=",
-  "debian pkg search": "https://packages.debian.org/search?keywords=",
-  "binge-browser": "https://binge-browser.vercel.app/?search_query=",
-  rule34: "https://rule34.xxx/index.php?page=post&s=list&tags=",
-};
-
 const timeEl = document.getElementById("time");
 const amEl = document.getElementById("am");
 const pmEl = document.getElementById("pm");
 const dateEl = document.getElementById("date");
-const searchEngineEl = document.getElementById("search-engine");
-const searchBarForm = document.getElementById("search-bar");
-
-for (let se in searchEngines) {
-  const option = document.createElement("option");
-  option.text = se;
-  option.value = searchEngines[se];
-  searchEngineEl.appendChild(option);
-}
 
 setInterval(
   (function setDatetime() {
@@ -75,14 +54,5 @@ setInterval(
   })(),
   1000
 );
-
-searchBarForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const url = this["search-engine"].value;
-  const query = this["search-query"].value;
-
-  window.location.href = url + query;
-});
 
 registerServiceWorker();
